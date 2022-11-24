d1ff7e2c0564082f06fa342b0dc59aa3ca597f82
Thomas Stroobants <thomas@avenue.be>
2022-11-24 23:19:34 +0100

    added url de lijn rt and kern

--- morph-kgc/main.py
+++ morph-kgc/main.py
@@ -9,9 +9,12 @@ from google.protobuf.json_format import MessageToJson
 
 nmbs_url = "https://sncb-opendata.hafas.de/gtfs/static/c21ac6758dd25af84cca5b707f3cb3de"
 nmbs_rt_url = "https://sncb-opendata.hafas.de/gtfs/realtime/c21ac6758dd25af84cca5b707f3cb3de"
-de_lijn_url = "https://api.delijn.be/gtfs/v2/realtime?"
+de_lijn_rt_url = "https://api.delijn.be/gtfs/v2/realtime?"
+de_lijn_url = "https://api.delijn.be/DLKernOpenData/api/v1/"
+
 
 subscription_key = '9ebca7dad65d4c1bb7d4cb7729b3c967'
+subscription_key_kern = '78099eddec5a4c698e9424c53c46831e'
 
 headers = {
     # Request headers
@@ -44,5 +47,22 @@ def get_gtfs_rt_lijn():
     #write data to file
     open(f"de-lijn-data/de-lijn-rt-gtfs.json", "w").write(feed_json)
 
+def get_gtfs_rf_nmbs():
+    feed = gtfs_realtime_pb2.FeedMessage()
+    response = requests.get(nmbs_rt_url)
+    feed.ParseFromString(response.content)
+    feed_json = MessageToJson(feed)
+    print(feed_json)
+
+def get_gtfs_rt(url, params=None, headers=None, file_name):
+    feed = gtfs_realtime_pb2.FeedMessage()
+    response = requests.get(url, params=params, headers=headers)
+    feed.ParseFromString(response.content)
+    feed_json = MessageToJson(feed)
+    #write data to file
+    open(file_name, "w").write(feed_json)
+
+
 #download_files(nmbs_rt_url, "nmbs-rt-data/nmbs-rt-proto")
-get_gtfs_rt_lijn()
+# get_gtfs_rt_lijn()
+get_gtfs_rf_nmbs()
