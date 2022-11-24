1e2dc4a5809b1dbf17fcc9da375b6813732d177a
Thomas Stroobants <thomas@avenue.be>
2022-11-24 11:31:04 +0100

    added gtfs from de lijn

--- morph-kgc/main.py
+++ morph-kgc/main.py
@@ -4,7 +4,23 @@ import subprocess
 
 nmbs_url = "https://sncb-opendata.hafas.de/gtfs/static/c21ac6758dd25af84cca5b707f3cb3de"
 nmbs_rt_url = "https://sncb-opendata.hafas.de/gtfs/realtime/c21ac6758dd25af84cca5b707f3cb3de"
+de_lijn_url = "https://api.delijn.be/gtfs/v2/realtime?"
 
+subscription_key = '9ebca7dad65d4c1bb7d4cb7729b3c967'
+
+headers = {
+    # Request headers
+    'Ocp-Apim-Subscription-Key': f'{subscription_key}',
+}
+
+params = urllib.parse.urlencode({
+    # Request parameters
+    'json': 'false',
+    'delay': 'true',
+    'canceled': 'true',
+    'source': '{string}',
+    'gtfsversion': '{string}',
+})
 
 
 def download_files(url, file_name):
@@ -14,5 +30,13 @@ def download_files(url, file_name):
 
 def unzip_file(file_name, dst_folder):
     subprocess.run(["unzip", file_name, "-d", dst_folder])
+
+def get_gtfs_lijn():
+    feed = gtfs_realtime_pb2.FeedMessage()
+    response = requests.get('https://api.delijn.be/gtfs/v2/realtime?', params=params, headers=headers)
+    feed.ParseFromString(response.content)
+    for entity in feed.entity:
+    if entity.HasField('trip_update'):
+        print(entity)
 
 download_files(nmbs_rt_url, "nmbs-rt-data/nmbs-rt-proto")
