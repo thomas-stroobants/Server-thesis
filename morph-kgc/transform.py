72a3fd418dd00c0d5d480cbd80fd13476ab032ec
ThomasMacBookPro <thomas@avenue.be>
2022-12-19 10:38:53 +0100

    added new rt files and changed scripts for new filenames. Updated subjectmap for entitymapping

--- morph-kgc/transform.py
+++ morph-kgc/transform.py
@@ -2,11 +2,12 @@ import json
 import time
 
 # my_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(1347517370))
-with open('nmbs-rt-data/nmbs-rt-gtfs.json') as sfile:
+with open('nmbs-rt-data/nmbs-rt-gtfs-new.json') as sfile:
     data = json.load(sfile)
 
 data["header"]["timestamp"] = time.strftime('%H:%M:%S', time.localtime(int(data["header"]["timestamp"])))
 
+# No need to transform departure/arrival times, the static data also uses posix time, this may however be needed for the development for the skill
 for item in data["entity"]:
     for tripdata in item["tripUpdate"]["stopTimeUpdate"]:
         if 'arrival' in tripdata:
@@ -14,5 +15,5 @@ for item in data["entity"]:
         if 'departure' in tripdata:
             tripdata['departure']['time'] = time.strftime('%H:%M:%S', time.localtime(int(tripdata['departure']['time'])))
 
-with open('nmbs-rt-json-adap.json', 'w') as ofile:
+with open('nmbs-rt-json-new-adap.json', 'w') as ofile:
     json.dump(data, ofile, indent = 2, sort_keys=True)
