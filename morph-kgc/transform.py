7d80669f50fef6619b95da0b2bc08521559d076f
ThomasMacBookPro <thomas@avenue.be>
2022-12-19 11:03:48 +0100

    adapted mapping for stoptimes, removed arrival time in iri to incorporate rt changes

--- morph-kgc/transform.py
+++ morph-kgc/transform.py
@@ -5,15 +5,15 @@ import time
 with open('nmbs-rt-data/nmbs-rt-gtfs-new.json') as sfile:
     data = json.load(sfile)
 
-data["header"]["timestamp"] = time.strftime('%H:%M:%S', time.localtime(int(data["header"]["timestamp"])))
+data["header"]["timestamp"] = time.strftime('%Y-%m-%dT%H:%M:%S', time.localtime(int(data["header"]["timestamp"])))
 
 # No need to transform departure/arrival times, the static data also uses posix time, this may however be needed for the development for the skill
-for item in data["entity"]:
-    for tripdata in item["tripUpdate"]["stopTimeUpdate"]:
-        if 'arrival' in tripdata:
-            tripdata['arrival']['time'] = time.strftime('%H:%M:%S', time.localtime(int(tripdata['arrival']['time'])))
-        if 'departure' in tripdata:
-            tripdata['departure']['time'] = time.strftime('%H:%M:%S', time.localtime(int(tripdata['departure']['time'])))
+# for item in data["entity"]:
+#     for tripdata in item["tripUpdate"]["stopTimeUpdate"]:
+#         if 'arrival' in tripdata:
+#             tripdata['arrival']['time'] = time.strftime('%H:%M:%S', time.localtime(int(tripdata['arrival']['time'])))
+#         if 'departure' in tripdata:
+#             tripdata['departure']['time'] = time.strftime('%H:%M:%S', time.localtime(int(tripdata['departure']['time'])))
 
 with open('nmbs-rt-json-new-adap.json', 'w') as ofile:
     json.dump(data, ofile, indent = 2, sort_keys=True)
