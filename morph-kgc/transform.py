e8419e1c3cad866930cf66a466a0f56d3bd223ac
ThomasMacBookPro <thomas@avenue.be>
2022-12-20 09:56:35 +0100

    added tripinfo to each stopupdate for easier iteration in rml

--- morph-kgc/transform.py
+++ morph-kgc/transform.py
@@ -5,15 +5,22 @@ import time
 with open('nmbs-rt-data/nmbs-rt-gtfs-new.json') as sfile:
     data = json.load(sfile)
 
-data["header"]["timestamp"] = time.strftime('%Y-%m-%dT%H:%M:%S', time.localtime(int(data["header"]["timestamp"])))
+    data["header"]["timestamp"] = time.strftime('%Y-%m-%dT%H:%M:%S', time.localtime(int(data["header"]["timestamp"])))
 
-# No need to transform departure/arrival times, the static data also uses posix time, this may however be needed for the development for the skill
-# for item in data["entity"]:
-#     for tripdata in item["tripUpdate"]["stopTimeUpdate"]:
-#         if 'arrival' in tripdata:
-#             tripdata['arrival']['time'] = time.strftime('%H:%M:%S', time.localtime(int(tripdata['arrival']['time'])))
-#         if 'departure' in tripdata:
-#             tripdata['departure']['time'] = time.strftime('%H:%M:%S', time.localtime(int(tripdata['departure']['time'])))
+    for item in data["entity"]:
+        trip_id = item["tripUpdate"]["trip"]["tripId"]
+        trip_startdate = item["tripUpdate"]["trip"]["startDate"]
+        trip_starttime = item["tripUpdate"]["trip"]["startTime"]
+        for tripdata in item["tripUpdate"]["stopTimeUpdate"]:
+            #adding tripdata to stoptimeupdate --> creates large duplicates
+            tripdata['tripId'] = trip_id
+            tripdata['tripStartDate'] = trip_startdate
+            tripdata['tripStartTime'] = trip_starttime
+            #adapting the time format from posix to hh:mm:ss 
+            if 'arrival' in tripdata:
+                tripdata['arrival']['time'] = time.strftime('%H:%M:%S', time.localtime(int(tripdata['arrival']['time'])))
+            if 'departure' in tripdata:
+                tripdata['departure']['time'] = time.strftime('%H:%M:%S', time.localtime(int(tripdata['departure']['time'])))
 
 with open('nmbs-rt-json-new-adap.json', 'w') as ofile:
     json.dump(data, ofile, indent = 2, sort_keys=True)
