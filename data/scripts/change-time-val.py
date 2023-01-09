52807944f9cd327102df14d1abadb9f7245a2941
Thomas Ubuntu VM <thomas@avenue.be>
2023-01-09 23:33:05 +0100

    added test scripts for rdflib and oxigraph and modified change-time script

--- data/scripts/change-time-val.py
+++ data/scripts/change-time-val.py
@@ -1,7 +1,18 @@
-with open('../graphs/knowledge-graph-nmbs.nt', 'r') as sfile:
+with open('./graphs/knowledge-graph-nmbs.nt', 'r') as sfile:
     filedata = sfile.read()
 
-filedata = filedata.replace('\"00:', '\"0:')
+filedata = filedata.replace('\"0:', '\"00:')
+filedata = filedata.replace('\"24:', '\"00:')
+# filedata = filedata.replace('\"01:', '\"1:')
+# filedata = filedata.replace('\"02:', '\"2:')
+# filedata = filedata.replace('\"03:', '\"3:')
+# filedata = filedata.replace('\"04:', '\"4:')
+# filedata = filedata.replace('\"05:', '\"5:')
+# filedata = filedata.replace('\"06:', '\"6:')
+# filedata = filedata.replace('\"07:', '\"7:')
+# filedata = filedata.replace('\"08:', '\"8:')
+# filedata = filedata.replace('\"09:', '\"9:')
 
-with open('../graphs/knowledge-graph-nmbs.nt', 'w') as dfile:
+
+with open('./graphs/knowledge-graph-nmbs.nt', 'w') as dfile:
     dfile.write(filedata)
