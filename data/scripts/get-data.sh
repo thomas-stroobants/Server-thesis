1174cbfce3e98d10ea987d0f2a02745194673a36
Thomas Ubuntu VM <thomas@avenue.be>
2023-01-09 14:48:50 +0100

    updated relative path

--- data/scripts/get-data.sh
+++ data/scripts/get-data.sh
@@ -1,18 +1,18 @@
 #! /usr/bin/bash
 
-DIR=( "../de-lijn-gtfs/" "../nmbs-gtfs/" "../mivb-gtfs/" "../tec-gtfs/" )
+DIR=( "./de-lijn-gtfs/" "./nmbs-gtfs/" "./mivb-gtfs/" "./tec-gtfs/" )
 
 
 #download file
 function download_data() {
     curl -O https://gtfs.irail.be/de-lijn/de_lijn-gtfs.zip
     curl -O https://gtfs.irail.be/mivb/mivb-gtfs.zip
-    curl -o "../nmbs-gtfs.zip" https://sncb-opendata.hafas.de/gtfs/static/c21ac6758dd25af84cca5b707f3cb3de 
+    curl -o "./nmbs-gtfs.zip" https://sncb-opendata.hafas.de/gtfs/static/c21ac6758dd25af84cca5b707f3cb3de 
     curl -O https://gtfs.irail.be/tec/tec-gtfs.zip
 
-    curl -O https://raw.githubusercontent.com/iRail/stations/master/stations.csv
-    curl -O https://raw.githubusercontent.com/iRail/stations/master/stops.csv
-    curl -O https://raw.githubusercontent.com/iRail/stations/master/facilities.csv
+    curl -O https://raw.githubusercontent.com/iRail/stations/master/stations.csv -d ./iRail
+    curl -O https://raw.githubusercontent.com/iRail/stations/master/stops.csv -d ./iRail
+    curl -O https://raw.githubusercontent.com/iRail/stations/master/facilities.csv -d ./iRail
 }
 
 function unzip_data() {
