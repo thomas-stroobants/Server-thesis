078f191e09c1f25014459ca2e60bebcf18be9f88
Thomas Ubuntu VM <thomas@avenue.be>
2023-01-12 17:49:41 +0100

    adapted get-data bash script

--- data/scripts/get-data.sh
+++ data/scripts/get-data.sh
@@ -5,10 +5,10 @@ DIR=( "./data/de-lijn-gtfs-$(date +'%d-%m-%Y')/" "./data/nmbs-gtfs-$(date +'%d-%
 
 #download file
 function download_data() {
-    curl -O https://gtfs.irail.be/de-lijn/de_lijn-gtfs.zip
-    curl -O https://gtfs.irail.be/mivb/mivb-gtfs.zip
+    curl -o "./data/" https://gtfs.irail.be/de-lijn/de_lijn-gtfs.zip
+    curl -o "./data/" https://gtfs.irail.be/mivb/mivb-gtfs.zip
     curl -o "./data/nmbs-gtfs.zip" https://sncb-opendata.hafas.de/gtfs/static/c21ac6758dd25af84cca5b707f3cb3de 
-    curl -O https://gtfs.irail.be/tec/tec-gtfs.zip
+    curl -o "./data/" https://gtfs.irail.be/tec/tec-gtfs.zip
 
     curl -o "./data/iRail/stations-$(date +'%d-%m-%Y').csv" https://raw.githubusercontent.com/iRail/stations/master/stations.csv 
     curl -o "./data/iRail/stops-$(date +'%d-%m-%Y').csv" https://raw.githubusercontent.com/iRail/stations/master/stops.csv
@@ -25,10 +25,10 @@ function unzip_data() {
         fi
     done
 
-    unzip -o ./de_lijn-gtfs.zip -d ${DIR[0]}
-    unzip -o ./nmbs-gtfs.zip -d ${DIR[1]}
-    unzip -o ./mivb-gtfs.zip -d ${DIR[2]}
-    unzip -o ./tec-gtfs.zip -d ${DIR[3]}
+    unzip -o ./data/de_lijn-gtfs.zip -d ${DIR[0]}
+    unzip -o ./data/nmbs-gtfs.zip -d ${DIR[1]}
+    unzip -o ./data/mivb-gtfs.zip -d ${DIR[2]}
+    unzip -o ./data/tec-gtfs.zip -d ${DIR[3]}
 }
 
 function txt_to_csv() {
