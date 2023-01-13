8689e4ee874bbe6db04d69145209fa195cb4ba26
Thomas Ubuntu VM <thomas@avenue.be>
2023-01-13 16:59:52 +0100

    adapted get static data script

--- data/scripts/get-data.sh
+++ data/scripts/get-data.sh
@@ -5,10 +5,10 @@ DIR=( "./data/de-lijn-gtfs-$(date +'%d-%m-%Y')/" "./data/nmbs-gtfs-$(date +'%d-%
 
 #download file
 function download_data() {
-    curl -o "./data/" https://gtfs.irail.be/de-lijn/de_lijn-gtfs.zip
-    curl -o "./data/" https://gtfs.irail.be/mivb/mivb-gtfs.zip
+    curl -o "./data/de_lijn-gtfs.zip" https://gtfs.irail.be/de-lijn/de_lijn-gtfs.zip
+    curl -o "./data/mivb-gtfs.zip" https://gtfs.irail.be/mivb/mivb-gtfs.zip
     curl -o "./data/nmbs-gtfs.zip" https://sncb-opendata.hafas.de/gtfs/static/c21ac6758dd25af84cca5b707f3cb3de 
-    curl -o "./data/" https://gtfs.irail.be/tec/tec-gtfs.zip
+    curl -o "./data/tec-gtfs.zip" https://gtfs.irail.be/tec/tec-gtfs.zip
 
     curl -o "./data/iRail/stations-$(date +'%d-%m-%Y').csv" https://raw.githubusercontent.com/iRail/stations/master/stations.csv 
     curl -o "./data/iRail/stops-$(date +'%d-%m-%Y').csv" https://raw.githubusercontent.com/iRail/stations/master/stops.csv
