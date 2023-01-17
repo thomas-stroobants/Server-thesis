49f4804af34a6eebe0b0b850e778ef67e95dd4cd
Thomas Stroobants <thomas@avenue.be>
2023-01-17 16:18:57 +0100

    python script to change URI of facilities to stop_id for rml mapping

--- data/scripts/replace-irail.py
+++ data/scripts/replace-irail.py
@@ -2,13 +2,13 @@ import pandas as pd
 
 df = pd.read_csv("/home/thomas/data/iRail/facilities.csv")
 
-for index, row in df.iterrows():
-    old_value = row["URI"]
+for i in range(len(df.index)):
+    old_value = df.loc[i, "URI"]
     new_value = format(int(old_value.split('/')[-1]), '07d')
-    print(row["URI"], new_value)
+    print(old_value, new_value)
     print()
-    row["URI"] = new_value
+    df.loc[i, "URI"] = new_value
 
 
 
-df.to_csv("/home/thomas/data/iRail/facilities1.csv")
+df.to_csv("/home/thomas/data/iRail/facilities1.csv", index=False)
