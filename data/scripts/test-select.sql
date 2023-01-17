b47b56132b6e25b91925489e0a3ba2767e9ec29a
Thomas Stroobants <thomas@avenue.be>
2023-01-17 13:59:46 +0100

    added second line for test other stoptime

--- data/scripts/test-select.sql
+++ data/scripts/test-select.sql
@@ -1,3 +1,4 @@
 -- SPARQL SELECT * from <http://example.com/nmbs-rt> where {  ?s ?p ?o . FILTER(?s = <http://example.com/nmbs/stoptimes/88____%3A007%3A%3A8891702%3A8844628%3A41%3A1842%3A20230203-8892007>) } LIMIT 100;
 -- SPARQL SELECT * from <http://example.com/nmbs> where {  ?s ?p ?o . FILTER(?s = <http://example.com/nmbs/stoptimes/88____%3A007%3A%3A8891702%3A8844628%3A41%3A1842%3A20230203-8892007>) } LIMIT 100;
-SPARQL SELECT * from <http://example.com/nmbs> where { <http://example.com/nmbs/stoptimes/88____%3A007%3A%3A8891702%3A8844628%3A41%3A1842%3A20230203-8892007> ?p ?o . };
+SPARQL SELECT * from <http://example.com/nmbs> where { <http://example.com/nmbs/stoptimes/88____%3A046%3A%3A8896008%3A8892601%3A4%3A1614%3A20231208-8892601> ?p ?o . };
+SPARQL SELECT * from <http://example.com/nmbs> where { <http://example.com/nmbs/stoptimes/88____%3A097%3A%3A8819406%3A8872009%3A27%3A1522%3A20231208-8814266> ?p ?o . };
