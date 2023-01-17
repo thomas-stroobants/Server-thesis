ea58dc58b3b7f58e30c8ac7186cecbc28d083e64
Thomas Stroobants <thomas@avenue.be>
2023-01-17 13:07:56 +0100

    test version of update modify sql file

--- data/scripts/test-update.sql
+++ data/scripts/test-update.sql
@@ -1,1 +1,32 @@
 --test for update graph with rt data
+SPARQL
+PREFIX gtfs: <http://vocab.gtfs.org/terms#>
+PREFIX dct: <http://purl.org/dc/terms/>
+PREFIX example: <http://example.com/>
+
+MODIFY example:nmbs
+DELETE
+    {
+        ?s dct:date ?d .
+        ?s gtfs:arrivalTime ?a .
+        ?s gtfs:departureTime ?dt .
+        ?s gtfs:stop ?st .
+    }
+INSERT
+    {
+        ?s dct:date ?nd .
+        ?s gtfs:arrivalTime ?na .
+        ?s gtfs:departureTime ?ndt .
+        ?s gtfs:stop ?nst .
+    }
+WHERE
+    {
+        GRAPH example:nmbs
+            {
+                ?s gtfs:stop ?st .
+            }
+        GRAPH example:nmbs-rt
+            {
+                ?s gtfs:stop ?st .
+            }
+    }
