7dd54b5bd0b04aa16af53d188f1084c639ac27a6
Thomas Stroobants <thomas@avenue.be>
2023-01-17 13:11:17 +0100

    updated test-select file to search for one specific trip delay

--- data/scripts/test-select.sql
+++ data/scripts/test-select.sql
@@ -1,1 +1,1 @@
-SPARQL SELECT * from <http://example.com/nmbs> where { ?s ?p ?o } LIMIT 100;
+SPARQL SELECT * from <http://example.com/nmbs> where { <http://example.com/nmbs/stoptimes/88____%3A007%3A%3A8891702%3A8844628%3A41%3A1842%3A20230203-8892007> ?p ?o } LIMIT 100;
