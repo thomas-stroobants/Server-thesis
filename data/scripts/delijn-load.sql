44532d9eff5f0f3b6d2d76f061dc2bdeac9dd83b
Thomas Stroobants <thomas@avenue.be>
2023-01-17 11:14:54 +0100

    added file for loading static data from delijn, renamed file for nmbs and added comments to file for clearing bulk loader table

---
+++ data/scripts/delijn-load.sql
@@ -0,0 +1,8 @@
+--File for testing De Lijn loading static data and changing with realtime data
+
+ld_dir('/home/thomas/graphs', 'knowledge-graph-delijn.nt', 'http://example.com/de-lijn');
+-- select * from DB.DBA.load_list;
+rdf_loader_run();
+checkpoint;
+checkpoint_interval(60);
+scheduler_interval(10);
--- data/scripts/nmbs-load.sql
+++ data/scripts/nmbs-load.sql
@@ -1,7 +1,7 @@
 --File for testing NMBS loading static data and changing with realtime data
 
 ld_dir('/home/thomas/graphs', 'knowledge-graph-nmbs.nt', 'http://example.com/nmbs');
-select * from DB.DBA.load_list;
+-- select * from DB.DBA.load_list;
 rdf_loader_run();
 checkpoint;
 checkpoint_interval(60);
