2352a39541a221e081e4bf0231fcccd9192d0d0b
Thomas Stroobants <thomas@avenue.be>
2023-01-20 12:37:58 +0100

    second revision of rt mapping de lijn, adapted structure from json file. Cancelled trips still not working

--- data/scripts/transform-dl.py
+++ data/scripts/transform-dl.py
@@ -2,7 +2,7 @@ import json
 import time
 import os
 
-with open(f'{os.environ.get('HOME')}/data/de-lijn-rt-data/de-lijn-rt-gtfs-new.json') as sfile:
+with open("/home/thomas/data/de-lijn-rt-data/de-lijn-rt-gtfs.json") as sfile:
     data = json.load(sfile)
 
     data["header"]["timestamp"] = time.strftime('%Y-%m-%dT%H:%M:%S', time.localtime(int(data["header"]["timestamp"])))
@@ -10,10 +10,15 @@ with open(f'{os.environ.get('HOME')}/data/de-lijn-rt-data/de-lijn-rt-gtfs-new.js
     for item in data["entity"]:
         if 'startDate' in item["tripUpdate"]["trip"]:
             item["tripUpdate"]["trip"]['startDate'] = time.strftime('%Y-%m-%d', time.localtime(int(item["tripUpdate"]["trip"]['startDate'])))
-            
+            item["tripUpdate"]["trip"]['timestamp'] = time.strftime('%H:%M:%S', time.localtime(int(item['tripUpdate']['timestamp'])))
+        if 'stopTimeUpdate' in item["tripUpdate"]:
+            for it in item["tripUpdate"]["stopTimeUpdate"]:
+                it["timestamp"] = time.strftime('%H:%M:%S', time.localtime(int(item['tripUpdate']['timestamp'])))
+                it["tripId"] = item['tripUpdate']['trip']['tripId']
+
         #adapting the time format from posix to hh:mm:ss 
-        item['tripUpdate']['timestamp'] = time.strftime('%H:%M:%S', time.localtime(int(item['tripUpdate']['timestamp'])))
+        # item['tripUpdate']['timestamp'] = time.strftime('%H:%M:%S', time.localtime(int(item['tripUpdate']['timestamp'])))
         
 
-with open(f'{os.environ.get('HOME')}/data/de-lijn-rt-data/de-lijn-rt-gtfs-new-adap.json', 'w') as ofile:
+with open('/home/thomas/data/de-lijn-rt-data/de-lijn-rt-gtfs-new-adap.json', 'w') as ofile:
     json.dump(data, ofile, indent = 2, sort_keys=True)
