1c89f83c608ff61e3d67e30fafe0f5170936185a
Thomas Desktop <thomas@avenue.be>
2023-01-25 12:49:50 +0100

    added sql files for de-lijn rt data load and added rt graphs to delete graph sql files

--- data/scripts/query/delete-nmbs-graph.sql
+++ data/scripts/query/delete-nmbs-graph.sql
@@ -1,2 +1,3 @@
 --file to delete old GTFS data from NMBS
 SPARQL CLEAR GRAPH <http://example.com/nmbs>;
+SPARQL CLEAR GRAPH <http://example.com/nmbs-rt>;
---
+++ data/scripts/query/test-update-dl.sql
@@ -0,0 +1,28 @@
+--test for update graph with rt data
+SPARQL
+PREFIX gtfs: <http://vocab.gtfs.org/terms#>
+-- PREFIX dct: <http://purl.org/dc/terms/>
+-- PREFIX example: <http://example.com/>
+
+MODIFY <http://example.com/de-lijn>
+DELETE
+    {
+        ?s gtfs:departureTime ?dt .
+    }
+INSERT
+    {
+        ?s gtfs:departureTime ?ndt .
+    }
+WHERE
+    {
+        GRAPH <http://example.com/de-lijn>
+            {
+                ?s gtfs:stop ?st .
+                ?s gtfs:departureTime ?dt .
+            }
+        GRAPH <http://example.com/de-lijn-rt>
+            {
+                ?s gtfs:stop ?nst .
+                ?s gtfs:departureTime ?ndt .
+            }
+    };
