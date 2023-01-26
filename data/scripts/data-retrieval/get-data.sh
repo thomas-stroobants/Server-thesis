00619f058d4ab082e3634d23d58edfc23d9b59bd
Thomas Desktop <thomas@avenue.be>
2023-01-26 11:56:40 +0100

    new data from iRail and adapted mivb mapping to ignore shapes

--- data/scripts/data-retrieval/get-data.sh
+++ data/scripts/data-retrieval/get-data.sh
@@ -1,18 +1,18 @@
 #! /usr/bin/bash
 
-DIR=( "$DATAPATH/data/de-lijn-gtfs-$(date +'%d-%m-%Y')/" "$DATAPATH/data/nmbs-gtfs-$(date +'%d-%m-%Y')/" "$DATAPATH/data/mivb-gtfs-$(date +'%d-%m-%Y')/" "$DATAPATH/data/tec-gtfs-$(date +'%d-%m-%Y')/" )
+DIR=( "$HOME/data/de-lijn-gtfs-$(date +'%d-%m-%Y')/" "$HOME/data/nmbs-gtfs-$(date +'%d-%m-%Y')/" "$HOME/data/mivb-gtfs-$(date +'%d-%m-%Y')/" "$HOME/data/tec-gtfs-$(date +'%d-%m-%Y')/" )
 
 
 #download file
 function download_data() {
-    curl -o "$DATAPATH/data/de_lijn-gtfs.zip" https://gtfs.irail.be/de-lijn/de_lijn-gtfs.zip
-    curl -o "$DATAPATH/data/mivb-gtfs.zip" https://gtfs.irail.be/mivb/mivb-gtfs.zip
-    curl -o "$DATAPATH/data/nmbs-gtfs.zip" https://sncb-opendata.hafas.de/gtfs/static/c21ac6758dd25af84cca5b707f3cb3de 
-    curl -o "$DATAPATH/data/tec-gtfs.zip" https://gtfs.irail.be/tec/tec-gtfs.zip
+    curl -o "$HOME/data/de_lijn-gtfs.zip" https://gtfs.irail.be/de-lijn/de_lijn-gtfs.zip
+    curl -o "$HOME/data/mivb-gtfs.zip" https://gtfs.irail.be/mivb/mivb-gtfs.zip
+    curl -o "$HOME/data/nmbs-gtfs.zip" https://sncb-opendata.hafas.de/gtfs/static/c21ac6758dd25af84cca5b707f3cb3de 
+    curl -o "$HOME/data/tec-gtfs.zip" https://gtfs.irail.be/tec/tec-gtfs.zip
 
-    curl -o "$DATAPATH/data/iRail/stations-$(date +'%d-%m-%Y').csv" https://raw.githubusercontent.com/iRail/stations/master/stations.csv 
-    curl -o "$DATAPATH/data/iRail/stops-$(date +'%d-%m-%Y').csv" https://raw.githubusercontent.com/iRail/stations/master/stops.csv
-    curl -o "$DATAPATH/data/iRail/facilities-$(date +'%d-%m-%Y').csv" https://raw.githubusercontent.com/iRail/stations/master/facilities.csv
+    curl -o "$HOME/data/iRail/stations-$(date +'%d-%m-%Y').csv" https://raw.githubusercontent.com/iRail/stations/master/stations.csv 
+    curl -o "$HOME/data/iRail/stops-$(date +'%d-%m-%Y').csv" https://raw.githubusercontent.com/iRail/stations/master/stops.csv
+    curl -o "$HOME/data/iRail/facilities-$(date +'%d-%m-%Y').csv" https://raw.githubusercontent.com/iRail/stations/master/facilities.csv
 }
 
 function unzip_data() {
@@ -25,10 +25,10 @@ function unzip_data() {
         fi
     done
 
-    unzip -o $DATAPATH/data/de_lijn-gtfs.zip -d ${DIR[0]}
-    unzip -o $DATAPATH/data/nmbs-gtfs.zip -d ${DIR[1]}
-    unzip -o $DATAPATH/data/mivb-gtfs.zip -d ${DIR[2]}
-    unzip -o $DATAPATH/data/tec-gtfs.zip -d ${DIR[3]}
+    unzip -o $HOME/data/de_lijn-gtfs.zip -d ${DIR[0]}
+    unzip -o $HOME/data/nmbs-gtfs.zip -d ${DIR[1]}
+    unzip -o $HOME/data/mivb-gtfs.zip -d ${DIR[2]}
+    unzip -o $HOME/data/tec-gtfs.zip -d ${DIR[3]}
 }
 
 function txt_to_csv() {
