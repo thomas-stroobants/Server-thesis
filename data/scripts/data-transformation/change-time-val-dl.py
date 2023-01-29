88598c2401fbc64825fd61c443a63d03d3af40cf
Thomas Desktop <thomas@avenue.be>
2023-01-29 21:31:30 +0100

    added python script to change hour format of de-lijn rt data and updated delete-queries for better performance. Added aliases for the isql commands

---
+++ data/scripts/data-transformation/change-time-val-dl.py
@@ -0,0 +1,18 @@
+with open(f'./graphs/knowledge-graph-delijn.nt', 'r') as sfile:
+    filedata = sfile.read()
+
+filedata = filedata.replace('\"0:', '\"00:')
+filedata = filedata.replace('\"24:', '\"00:')
+# filedata = filedata.replace('\"01:', '\"1:')
+# filedata = filedata.replace('\"02:', '\"2:')
+# filedata = filedata.replace('\"03:', '\"3:')
+# filedata = filedata.replace('\"04:', '\"4:')
+# filedata = filedata.replace('\"05:', '\"5:')
+# filedata = filedata.replace('\"06:', '\"6:')
+# filedata = filedata.replace('\"07:', '\"7:')
+# filedata = filedata.replace('\"08:', '\"8:')
+# filedata = filedata.replace('\"09:', '\"9:')
+
+
+with open(f'./graphs/knowledge-graph-delijn.nt', 'w') as dfile:
+    dfile.write(filedata)
