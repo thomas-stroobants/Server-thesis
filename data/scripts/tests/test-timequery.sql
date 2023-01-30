dcabffe551f9486ea44f1479bc13509e4e15c0bf
Thomas Desktop <thomas@avenue.be>
2023-01-30 13:53:52 +0100

    removed unnecessary line from query

--- data/scripts/tests/test-timequery.sql
+++ data/scripts/tests/test-timequery.sql
@@ -2,9 +2,8 @@ SPARQL
 PREFIX gtfs: <http://vocab.gtfs.org/terms#>
 PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
 
-SELECT ?subject ?predicate ?object
+SELECT ?subject ?time
 WHERE {
-  ?subject ?predicate ?object .
   ?subject gtfs:departureTime ?time .
   FILTER (?time >= "08:00:00"^^xsd:time && ?time <= "10:00:00"^^xsd:time)
 }
