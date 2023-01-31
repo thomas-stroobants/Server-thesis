b33e5651fe36b583a24b07b13251b44a0b080567
Thomas Desktop <thomas@avenue.be>
2023-01-31 16:27:55 +0100

    changed filename of output file to replace the existing file stop_times.csv

--- data/scripts/data-transformation/connect-datetime.py
+++ data/scripts/data-transformation/connect-datetime.py
@@ -63,4 +63,4 @@ for index, row in stop_times_df.iterrows():
         stop_times_df.at[index, 'departure_time'] = mod_departure_time.isoformat()
 
 # write results to new file
-stop_times_df.to_csv('/home/thomas/data/nmbs-gtfs/stop_times_mod.csv', index=False)
+stop_times_df.to_csv('/home/thomas/data/nmbs-gtfs/stop_times.csv', index=False)
