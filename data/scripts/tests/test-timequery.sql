0e036817e7c4e0fe078f7e2949cdde6e0587f326
Thomas Desktop <thomas@avenue.be>
2023-01-31 16:59:43 +0100

    new logs for mappings, adapted timequery for xsd:dateTime

--- data/scripts/tests/test-timequery.sql
+++ data/scripts/tests/test-timequery.sql
@@ -5,6 +5,6 @@ PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
 SELECT ?subject ?time
 WHERE {
   ?subject gtfs:departureTime ?time .
-  FILTER (?time >= "08:00:00"^^xsd:time && ?time <= "10:00:00"^^xsd:time)
+  FILTER (?time >= "2023-01-31T08:00:00"^^xsd:dateTime && ?time <= "2023-01-31T10:00:00"^^xsd:dateTime)
 }
 ;
