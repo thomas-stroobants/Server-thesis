1e8052bcbe94cada4173736003f63b4c65a301cd
Thomas Desktop <thomas@avenue.be>
2023-02-01 21:35:43 +0100

    sql script for loading bluebike data

---
+++ data/scripts/query/bluebike-load.sql
@@ -0,0 +1,8 @@
+--File for testing BlueBike loading with realtime data
+
+ld_dir('/home/thomas/graphs', 'bluebike.ttl', 'http://example.com/blue-bike');
+-- select * from DB.DBA.load_list;
+rdf_loader_run();
+checkpoint;
+checkpoint_interval(60);
+scheduler_interval(10);
