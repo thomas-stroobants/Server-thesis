bca15093ffe6e937f01540f43d90a0d07d78f224
Thomas Desktop <thomas@avenue.be>
2023-02-02 11:29:24 +0100

    added comments and python scripts for transformation time to datetime

--- data/scripts/data-retrieval/get-data.sh
+++ data/scripts/data-retrieval/get-data.sh
@@ -1,15 +1,20 @@
 #! /usr/bin/bash
 
-DIR=( "$HOME/data/de-lijn-gtfs-$(date +'%d-%m-%Y')/" "$HOME/data/nmbs-gtfs-$(date +'%d-%m-%Y')/" "$HOME/data/mivb-gtfs-$(date +'%d-%m-%Y')/" "$HOME/data/tec-gtfs-$(date +'%d-%m-%Y')/" )
+# DIR=( "$HOME/data/de-lijn-gtfs-$(date +'%d-%m-%Y')/" "$HOME/data/nmbs-gtfs-$(date +'%d-%m-%Y')/" "$HOME/data/mivb-gtfs-$(date +'%d-%m-%Y')/" "$HOME/data/tec-gtfs-$(date +'%d-%m-%Y')/" )
+DIR=(   "$HOME/data/de-lijn-gtfs-$(date +'%d-%m-%Y')/" 
+        "$HOME/data/nmbs-gtfs-$(date +'%d-%m-%Y')/" 
+        # "$HOME/data/mivb-gtfs-$(date +'%d-%m-%Y')/" 
+        # "$HOME/data/tec-gtfs-$(date +'%d-%m-%Y')/" 
+    )
 
 
 #download file
 function download_data() {
     # Download gtfs data from de lijn, mivb, nmbs and tec
     curl -o "$HOME/data/de_lijn-gtfs.zip" https://gtfs.irail.be/de-lijn/de_lijn-gtfs.zip
-    curl -o "$HOME/data/mivb-gtfs.zip" https://gtfs.irail.be/mivb/mivb-gtfs.zip
     curl -o "$HOME/data/nmbs-gtfs.zip" https://sncb-opendata.hafas.de/gtfs/static/c21ac6758dd25af84cca5b707f3cb3de 
-    curl -o "$HOME/data/tec-gtfs.zip" https://gtfs.irail.be/tec/tec-gtfs.zip
+    # curl -o "$HOME/data/mivb-gtfs.zip" https://gtfs.irail.be/mivb/mivb-gtfs.zip
+    # curl -o "$HOME/data/tec-gtfs.zip" https://gtfs.irail.be/tec/tec-gtfs.zip
 
     # Download NMBS station information from iRail
     curl -o "$HOME/data/iRail/stations-$(date +'%d-%m-%Y').csv" https://raw.githubusercontent.com/iRail/stations/master/stations.csv 
@@ -30,8 +35,8 @@ function unzip_data() {
 
     unzip -o $HOME/data/de_lijn-gtfs.zip -d ${DIR[0]}
     unzip -o $HOME/data/nmbs-gtfs.zip -d ${DIR[1]}
-    unzip -o $HOME/data/mivb-gtfs.zip -d ${DIR[2]}
-    unzip -o $HOME/data/tec-gtfs.zip -d ${DIR[3]}
+    # unzip -o $HOME/data/mivb-gtfs.zip -d ${DIR[2]}
+    # unzip -o $HOME/data/tec-gtfs.zip -d ${DIR[3]}
 }
 
 # Convert all .txt files to .csv
@@ -46,6 +51,22 @@ function txt_to_csv() {
 }
 
 # Process
+echo "Starting process of data retrieval and transformation of GTFS static data.."
+echo "Downloading data from servers.."
 download_data
+echo "Extract files from ZIP folders.."
 unzip_data
+echo "Changing file extensions from .txt to .csv.."
 txt_to_csv
+
+# Activate virtual environment for Python
+seba
+
+# Change departuretimes from time format to ISO dateTime
+python3 $HOME/data/scripts/data-transformation/connect-datetime.py &
+python3 $HOME/data/scripts/data-transformation/connect-datetime-dl.py 
+
+echo "data transformation complete?"
+
+echo "transforming iRail data"
+python3 $HOME/data/scripts/data-transformation/replace-irail.py
