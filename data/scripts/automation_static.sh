5ef10f47c5d5a82a797c5f5c0ee45313d4e33131
Thomas Desktop <thomas@avenue.be>
2023-02-03 15:56:48 +0100

    first draft of complete automation script for static data, changed log file from delijn

--- data/scripts/automation_static.sh
+++ data/scripts/automation_static.sh
@@ -2,9 +2,10 @@
 
 # Activate virtual environment for Python
 source $HOME/env/bin/activate
-
+echo ""
 # Bash script to automate the data retrieval and conversion process, including loading data into Virtuoso
-echo "----------------------------------------------"
+echo "==================================================================="
+echo ""
 start_time=$(date +%s) 
 echo "$(date) | Start of data retrieval static data"
 # Download the static data
@@ -12,11 +13,16 @@ $HOME/data/scripts/data-retrieval/get-data.sh
 
 
 # Delete NMBS data from Virtuoso
-isql 1111 dba dba ~/data/scripts/query/delete-nmbs-graph.sql &
+isql 1111 dba dba ~/data/scripts/query/clear-bulk-load-list.sql ~/data/scripts/query/delete-nmbs-graph.sql &
 pid_delete_nmbs=$!
-echo "PID is $pid_delete_nmbs"
+echo "$(date) | PID isql delete nmbs is $pid_delete_nmbs"
 
-wait $pid_delete_nmbs
+# Delete NMBS data from Virtuoso
+isql 1111 dba dba ~/data/scripts/query/delete-delijn-graph.sql &
+pid_delete_delijn=$!
+echo "$(date) | PID isql delete delijn is $pid_delete_delijn"
+
+# wait $pid_delete_nmbs $pid_delete_delijn
 
 # Change departuretimes from time format to ISO dateTime
 python3 $HOME/data/scripts/data-transformation/connect-datetime.py &
@@ -25,14 +31,34 @@ python3 $HOME/data/scripts/data-transformation/connect-datetime-dl.py &
 pid_dl=$!
 echo "$(date) | Waiting for PID NMBS ($pid_nmbs) and De Lijn ($pid_dl) to finish"
 
-wait $pid_nmbs $pid_dl
+wait $pid_nmbs $pid_dl $pid_delete_nmbs $pid_delete_delijn
 echo "$(date) | data transformation complete"
 
 echo "$(date) | transforming iRail data"
 python3 $HOME/data/scripts/data-transformation/replace-irail.py
 
 
+# Transforming the data to KG
+echo "$(date) | Starting materialization of NMBS data using Morph-KGC"
+python3 -m morph_kgc ~/graphs/config/config-nmbs.ini &
+pid_morph_nmbs=$!
 
+echo "$(date) | Starting materialization of NMBS data using Morph-KGC"
+python3 -m morph_kgc ~/graphs/config/config-dl.ini &
+pid_morph_delijn=$!
+
+
+wait $pid_morph_nmbs $pid_morph_delijn
+echo "$(date) | Materialization complete"
+
+# Loading the data into Virtuoso
+isql 1111 dba dba ~/data/scripts/query/nmbs-load.sql &
+pid_load_nmbs=$!
+isql 1111 dba dba ~/data/scripts/query/delijn-load.sql &
+pid_load_delijn=$!
+
+wait $pid_load_nmbs $pid_load_delijn
+echo "$(date) | Loading data into Virtuoso complete"
 
 stop_time=$(date +%s)
 
