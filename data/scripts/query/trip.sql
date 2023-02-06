426866e9f89d3f71a47a949d773fc9fdb44e80c8
Thomas Desktop <thomas@avenue.be>
2023-02-06 20:27:13 +0100

    automation for realtime start and start sql file for query trips

---
+++ data/scripts/query/trip.sql
@@ -0,0 +1,21 @@
+SPARQL
+
+PREFIX rr: <http://www.w3.org/ns/r2rml#> .
+PREFIX rml: <http://semweb.mmlab.be/ns/rml#> .
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+PREFIX ql: <http://semweb.mmlab.be/ns/ql#> .
+PREFIX map: <http://mapping.example.com/> .
+PREFIX ma: <http://www.w3.org/ns/ma-ont#> .
+PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> .
+PREFIX sd: <http://www.w3.org/ns/sparql-service-description#> .
+PREFIX dc: <http://purl.org/dc/elements/1.1/> .
+PREFIX foaf: <http://xmlns.com/foaf/0.1/> .
+PREFIX rev: <http://purl.org/stuff/rev#> .
+PREFIX v: <http://rdf.data-vocabulary.org/#> .
+PREFIX schema: <http://schema.org/> .
+PREFIX gtfs: <http://vocab.gtfs.org/terms#> .
+PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#> .
+PREFIX dct: <http://purl.org/dc/terms/> .
+
+SELECT 
