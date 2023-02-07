acd21a5003e9d04f4de1f9fcfb779d8c9fa921b8
Thomas Desktop <thomas@avenue.be>
2023-02-07 22:52:45 +0100

    first test for delijn to access stop using haltenummer

--- data/scripts/tests/test-traject.sql
+++ data/scripts/tests/test-traject.sql
@@ -3,3 +3,9 @@ SPARQL
 PREFIX gtfs: <http://vocab.gtfs.org/terms#>
 -- PREFIX dct: <http://purl.org/dc/terms/>
 -- PREFIX example: <http://example.com/>
+SELECT * from <http://example.com/de-lijn> 
+where { 
+    ?s ?p ?o .
+    ?s gtfs:code "204391" . 
+} 
+LIMIT 20;
