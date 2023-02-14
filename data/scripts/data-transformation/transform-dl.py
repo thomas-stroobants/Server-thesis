b18817cd7f56c02fc3e9b175535f98519b9e312a
Thomas Desktop <thomas@avenue.be>
2023-02-14 15:01:31 +0100

    start modification of mappings for RT data

--- data/scripts/data-transformation/transform-dl.py
+++ data/scripts/data-transformation/transform-dl.py
@@ -1,11 +1,13 @@
 import json
 import time
 import os
+import datetime
+from pathlib import Path
 
-with open("/home/thomas/data/de-lijn-rt-data/de-lijn-rt-gtfs.json") as sfile:
+with open(f'{Path.home()}/data/de-lijn-rt-data/de-lijn-rt-gtfs.json') as sfile:
     data = json.load(sfile)
 
-    data["header"]["timestamp"] = time.strftime('%Y-%m-%dT%H:%M:%S', time.localtime(int(data["header"]["timestamp"])))
+    data["header"]["timestamp"] = datetime.datetime.fromtimestamp(int(data["header"]["timestamp"])).isoformat()
 
     for item in data["entity"]:
         # item["tripUpdate"]["stopTimeUpdate"][0]["timestamp"]=time.strftime('%H:%M:%S', time.localtime(int(item['tripUpdate']['timestamp'])))
@@ -18,20 +20,23 @@ with open("/home/thomas/data/de-lijn-rt-data/de-lijn-rt-gtfs.json") as sfile:
             # item["tripUpdate"]["trip"]['timestamp'] = time.strftime('%H:%M:%S', time.localtime(int(item['tripUpdate']['timestamp'])))
         if 'stopTimeUpdate' in item["tripUpdate"]:
             for it in item["tripUpdate"]["stopTimeUpdate"]:
-                it["timestamp"] = time.strftime('%H:%M:%S', time.localtime(int(item['tripUpdate']['timestamp'])))
-                it["tripId"] = item['tripUpdate']['trip']['tripId']
+                delay_seconds = it["departure"]["delay"]
+                if delay_seconds >= 0:
+                    it["departure"]["delay"] = time.strftime('P%HH%MM%SS', time.gmtime(delay_seconds))
+                else: 
+                    it["departure"]["delay"] = time.strftime('-P%HH%MM%SS', time.gmtime(abs(delay_seconds)))
 
         #adapting the time format from posix to hh:mm:ss 
         # item['tripUpdate']['timestamp'] = time.strftime('%H:%M:%S', time.localtime(int(item['tripUpdate']['timestamp'])))
         
 
-        elif 'startDate' in item["tripUpdate"]["trip"]:
-            new_data = [{ "timestamp" : time.strftime('%H:%M:%S', time.localtime(int(item['tripUpdate']['timestamp']))),
-                         "tripId" : item['tripUpdate']['trip']['tripId'],
-                         "scheduleRelationship" : item["tripUpdate"]["trip"]['scheduleRelationship'] }]
-            stopTime = { "tripInfo" : new_data}
-            item["tripUpdate"]["tripInfo"] = new_data
+        # elif 'startDate' in item["tripUpdate"]["trip"]:
+        #     new_data = [{ "timestamp" : time.strftime('%H:%M:%S', time.localtime(int(item['tripUpdate']['timestamp']))),
+        #                  "tripId" : item['tripUpdate']['trip']['tripId'],
+        #                  "scheduleRelationship" : item["tripUpdate"]["trip"]['scheduleRelationship'] }]
+        #     stopTime = { "tripInfo" : new_data}
+        #     item["tripUpdate"]["tripInfo"] = new_data
             
 
-with open('/home/thomas/data/de-lijn-rt-data/de-lijn-rt-gtfs-new-adap.json', 'w') as ofile:
+with open(f'{Path.home()}/data/de-lijn-rt-data/de-lijn-rt-gtfs-adap.json', 'w') as ofile:
     json.dump(data, ofile, indent = 2, sort_keys=True)
