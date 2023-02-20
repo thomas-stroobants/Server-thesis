1ed58bc350abf356a347a44e2ecb1a3c625d6597
Thomas Desktop <thomas@avenue.be>
2023-02-20 19:50:39 +0100

    only delete entries that are finished loading/ changed paths of graphs to be loaded

--- data/scripts/query/clear-bulk-load-list.sql
+++ data/scripts/query/clear-bulk-load-list.sql
@@ -1,5 +1,5 @@
 -- clearing bluk load list, otherwise the next time you try to use the bulk loader with 
 -- a newer file with the same name it will think it has already done that task
-DELETE FROM DB.DBA.load_list;
--- DELETE FROM DB.DBA.load_list
--- WHERE ll_state = 2;
+-- DELETE FROM DB.DBA.load_list;
+DELETE FROM DB.DBA.load_list
+WHERE ll_state = 2;
--- data/scripts/query/nmbs-rt-load.sql
+++ data/scripts/query/nmbs-rt-load.sql
@@ -1,6 +1,6 @@
 --File for testing NMBS loading realtime data
 
-ld_dir('/home/thomas/graphs', 'knowledge-graph-nmbs-rt.nt', 'http://example.com/nmbs-rt');
+ld_dir('/home/thomas/graphs', 'knowledge-graph-nmbs-rt-test.nt', 'http://example.com/nmbs-rt');
 -- select * from DB.DBA.load_list;
 rdf_loader_run();
 checkpoint;
