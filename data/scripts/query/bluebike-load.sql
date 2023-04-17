191ba16cc58ad6d288098dc313a9207d442ce773
Thomas Desktop <thomas@avenue.be>
2023-04-17 08:13:35 +0200

    changed directory back to graphs due to access configuration for virtuoso

--- data/scripts/query/bluebike-load.sql
+++ data/scripts/query/bluebike-load.sql
@@ -1,6 +1,6 @@
 --File for testing BlueBike loading with realtime data
 
-ld_dir('/home/thomas/data/BlueBike', 'bluebike.ttl', 'http://example.com/blue-bike');
+ld_dir('/home/thomas/graphs/BlueBike', 'bluebike.ttl', 'http://example.com/blue-bike');
 -- select * from DB.DBA.load_list;
 rdf_loader_run();
 checkpoint;
