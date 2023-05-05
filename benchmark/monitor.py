68fa15f541a3450edd6b1182bafefe49712b424b
Thomas Desktop <thomas@avenue.be>
2023-05-05 14:47:51 +0200

    add first try of banchmark script

--- benchmark/monitor.py
+++ benchmark/monitor.py
@@ -1,4 +1,65 @@
 import psutil
+import time
+import subprocess
+import csv
+import threading
+import sys
+
+scripts = ["script1.sh", "schript2.py"]
+
+# get resources from psutil
+def get_resource_usage(pid):
+    process = psutil.Process(pid)
+    cpu_usage = process.cpu_percent()
+    memory_usage = process.memory_info().rss
+    return cpu_usage, memory_usage
+
+# 
+def check_resource_usage(pid):
+    cpu_usage, memory_usage = get_resource_usage(pid)
+    print(f"CPU usage: {cpu_usage}%")
+    print(f"Memory usage: {memory_usage/(1024**3):.2f}G")
+    # timer to repeat every 0.1 seconds
+    timer = threading.Timer(0.1, check_resource_usage, args=[pid])
+    timer.start()
+
+    if psutil.Process(pid).status() == psutil.STATUS_ZOMBIE:
+        timer.cancel()
+
+# list for subprocesses
+processes = []
+
+
+for script in scripts:
+    process = subprocess.Popen(script, shell=True)
+    pid = process.pid
+    start_time = time.time()
+    check_resource_usage(pid)
+    process.join()
+    stop_time = time.time()
+    processes.append((processes, pid ,start_time, stop_time))
+
+with open("data.csv", "w") as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(["Script", "Runtime", "CPU usage", "Memory usage"])
+    for script, pid, start_time in processes:
+        end_time = time.time()
+        runtime = end_time - start_time
+        print(f"Runtime: {runtime} seconds")
+        cpu_usage, memory_usage = get_resource_usage(pid)
+        print(f"CPU usage: {cpu_usage}%")
+        print(f"Memory usage: {memory_usage/(1024**3):.2f}G")
+        writer.writerow([script, runtime, cpu_usage, memory_usage])
 
 while True:
-    print(f"The memory usage is {psutil.cpu_percent(interval=0.1)}%")
+    # print(f"The memory usage is {psutil.cpu_percent(interval=0.1)}%") #use the interval to block the system for a more accurate value
+    # split_cpu = psutil.cpu_percent(percpu=True, interval=0.1)
+    # for idx, usage in enumerate(split_cpu):
+    #     print(f"CORE {idx+1}: {usage}%")
+    
+
+    mem_usage = psutil.memoru()
+
+    print(f"Free: {mem_usage.percent}%")
+    print(f"Total: {mem_usage.total/(1024**3):.2f}G")
+    print(f"Used: {mem_usage.used/(1024**3):.2f}G")
--- benchmark/monitor.sh
+++ benchmark/monitor.sh
@@ -1,2 +1,3 @@
 #! /usr/bin/bash
 
+top -b -d1 -o +%MEM | grep -A1 'KiB Mem' >> memory.log
