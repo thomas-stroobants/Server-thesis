c6fc995ce77413f3d48082f74d5f3f492ad155cc
Thomas Desktop <thomas@avenue.be>
2023-05-05 15:48:58 +0200

    changed folder name for graphs bench

--- data-bench/query/delijn-rt-load.sql
+++ data-bench/query/delijn-rt-load.sql
@@ -1,6 +1,6 @@
 --File for testing De Lijn loading static data and changing with realtime data
 
-ld_dir('/home/thomas/graphs', 'knowledge-graph-delijn-rt.nt', 'http://example.com/de-lijn-rt');
+ld_dir('/home/thomas/graphs-bench', 'knowledge-graph-delijn-rt.nt', 'http://example.com/de-lijn-rt');
 -- select * from DB.DBA.load_list;
 rdf_loader_run();
 checkpoint;
