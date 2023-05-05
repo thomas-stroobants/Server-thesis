07b1089441dde967fa6aa4f6743527aa2eac3e97
Thomas Desktop <thomas@avenue.be>
2023-05-05 16:50:14 +0200

    fix script

--- data-bench/data-retrieval/get-dat.sh
+++ data-bench/data-retrieval/get-dat.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/bash
-number = 100
-while $number > 10:
-    $number = $number - 1
+number=100
+while [ $number -gt 10 ]
+do
+    number=$(( $number - 1))
     echo "number is $number"
     sleep 1
+done
