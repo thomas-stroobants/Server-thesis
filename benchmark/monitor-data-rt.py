b0b14d116771d4504945535664a8fb2cb3385d80
Thomas Desktop <thomas@avenue.be>
2023-05-06 22:33:32 +0200

    added benchmark script that should work

---
+++ benchmark/monitor-data-rt.py
@@ -0,0 +1,59 @@
+import psutil, subprocess, time, csv, os
+
+scripts = [
+    f"{os.path.expanduser('~')}/data-bench/data-retrieval/get-data.sh",
+    ["python3", f"{os.path.expanduser('~')}/data-bench/data-retrieval/get_rt_data.py"],
+    ["python3", f"{os.path.expanduser('~')}/data-bench/data-transformation/replace-irail.py"],
+    ["python3", f"{os.path.expanduser('~')}/data-bench/data-transformation/connect-datetime.py"],
+    ["python3", f"{os.path.expanduser('~')}/data-bench/data-transformation/connect-datetime-dl.py"],
+    f"{os.path.expanduser('~')}/data-bench/data-transformation/test-split-stop.sh",
+
+]
+
+# proc = subprocess.Popen(["python3", f"{os.path.expanduser('~')}/data-bench/data-transformation/connect-datetime.py"])
+# process = psutil.Process(proc.pid)
+
+def get_cvs_filename(script):
+    filename = "test.csv"
+    if isinstance(script, list):    #for python scripts
+        filename = f"{os.path.expanduser('~')}/benchmark/bench-{os.path.splitext(os.path.basename(script[1]))[0]}.csv"
+    else:                           #for bash scripts
+        filename = f"{os.path.expanduser('~')}/benchmark/bench-{os.path.splitext(os.path.basename(script))[0]}.csv"
+
+    print(f"Filename is {filename}")
+    return filename
+
+def record_usage(proc, interval, filename):
+    records = []
+    process = psutil.Process(proc.pid)
+
+    while proc.poll() is None:
+        cpu_percent = process.cpu_percent(interval=interval)
+        mem_percent = process.memory_percent()
+        mem_mbytes = process.memory_info().rss / (1024 ** 2)
+        records.append([time.time(), cpu_percent, mem_percent, mem_mbytes])
+        time.sleep(interval)
+
+    with open(filename, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(['time', 'cpu_percent', 'memory_percent', 'memory_mbytes'])
+        writer.writerows(records)
+    
+    runtime = records[-1][0] - records[0][0]
+    print(f"Runtime is {runtime:.2f} seconds")
+
+
+for script in scripts:
+    filename = get_cvs_filename(script)
+    print(f"new filename: {filename} and script {script}")
+    # proc = subprocess.Popen(script)
+    # record_usage(proc, 0.3, filename)
+
+# while proc.poll() is None:
+#     cpu_usage.append(process.cpu_percent(interval=0.1))
+#     mem_usage.append(process.memory_info())
+
+# print(f"Average CPU: {sum(cpu_usage)/len(cpu_usage)}%")
+# print(f"Average mem: {sum(mem_usage)/len(mem_usage)/(1024**3):.2f} bytes")
+# print(f"{cpu_usage}")
+# print(f"{mem_usage}")
--- data-bench/data-retrieval/get-dat.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/bash
-number=100
-while [ $number -gt 10 ]
-do
-    number=$(( $number - 1))
-    echo "number is $number"
-    sleep 1
-done
