c04178b84bb2219525292e25f09bb4be79dfbd71
Thomas Desktop <thomas@avenue.be>
2023-05-07 22:17:26 +0200

    removed print statements and added virt mem usage

--- benchmark/monitor-data-rt.py
+++ benchmark/monitor-data-rt.py
@@ -20,26 +20,27 @@ def get_cvs_filename(script):
     else:                           #for bash scripts
         filename = f"{os.path.expanduser('~')}/benchmark/bench-{os.path.splitext(os.path.basename(script))[0]}.csv"
 
-    print(f"Filename is {filename}")
+    # print(f"Filename is {filename}")
     return filename
 
 def record_usage(proc, interval, filename):
     records = []
     process = psutil.Process(proc.pid)
 
-    start_time = time.time()
+    start_time = time.time_ns()
 
     while proc.poll() is None:
         cpu_percent = process.cpu_percent(interval=interval)
         mem_percent = process.memory_percent()
-        mem_mbytes = process.memory_info().rss / (1024 ** 2)
-        inter_time = round((time.time() - start_time), 5)
-        records.append([inter_time, cpu_percent, mem_percent, mem_mbytes])
-        time.sleep(interval)
+        mem_bytes = process.memory_info().rss #/ (1024 ** 2)
+        virt_mem_bytes = process.memory_info().vms #/ (1024 ** 2 )
+        inter_time = (time.time_ns() - start_time) / 1000000    #get milliseconds
+        records.append([round(inter_time), cpu_percent, mem_percent, mem_bytes, virt_mem_bytes])
+        # time.sleep(interval)
 
     with open(filename, 'w') as f:
         writer = csv.writer(f)
-        writer.writerow(['time', 'cpu_percent', 'memory_percent', 'memory_mbytes'])
+        writer.writerow(['time', 'cpu_percent', 'memory_percent', 'memory_mbytes', 'virt_mem_bytes'])
         writer.writerows(records)
     
     runtime = records[-1][0] - records[0][0]
@@ -48,11 +49,10 @@ def record_usage(proc, interval, filename):
 
 for script in scripts:
     filename = get_cvs_filename(script)
-    print(f"new filename: {filename} and script {script}")
     proc = subprocess.Popen(script)
-    record_usage(proc, 0.3, filename)
-    loadavg = [round((x / psutil.cpu_count() * 100),2) for x in psutil.getloadavg()]
-    print(f"Load average: {loadavg}")
+    record_usage(proc, 0.1, filename)
+    # loadavg = [round((x / psutil.cpu_count() * 100),2) for x in psutil.getloadavg()]
+    # print(f"Load average: {loadavg}")
 
 # while proc.poll() is None:
 #     cpu_usage.append(process.cpu_percent(interval=0.1))
