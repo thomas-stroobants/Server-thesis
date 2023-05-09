8aff87ded6bca82248e6a899549523bafb9b618c
Thomas Desktop <thomas@avenue.be>
2023-05-09 11:48:58 +0200

    changed for new top command

--- benchmark/monitor-virtuoso.sh
+++ benchmark/monitor-virtuoso.sh
@@ -19,8 +19,8 @@ check_bytes() {
     value=$1
     unit=${value:(-1)}
     if [ "$unit" == "g" ]; then
-        num=${value%$unit}
-        value=$((echo "$num * 1024 * 1024 " | bc))
+        num=$( echo ${value%$unit} | tr , . )
+        value=$(echo "$num * 1024 * 1024 " | bc)        
     fi
     echo $value
 }
@@ -33,15 +33,13 @@ monitor_virtuoso() {
     # Write headers to the CSV file
     echo "PID, Time, CPU %, Memory %, Memory Bytes, Virtual Memory Bytes" > $csv_file
 
-    # isql 1111 dba dba $isql_command &
-    # pid_isql=$!
-    # start_time=$(date +%s%N | cut -b1-13)
+    isql 1111 dba dba $isql_command &
+    pid_isql=$!
+    
     start_time=$(($(date +%s%N) / 1000000))
     # Loop to run the script and monitor performance every second
-    # while ps -p $pid_isql > /dev/null; do
-    while ps -p $virtuoso_pid > /dev/null; do
+    while ps -p $pid_isql > /dev/null; do
         # Get the current time stamp
-        # timestamp=$(date +%s%N | cut -b1-13)
         timestamp=$(($(date +%s%N) / 1000000))
         
         runtime=$(($timestamp - $start_time))
@@ -50,23 +48,26 @@ monitor_virtuoso() {
 
         while read -r line; do
             pid=$(echo "$line" | awk '{print $1}')
-            cpu_usage=$(echo "$line" | awk '{print $7}')
-            memory_usage=$(echo "$line" | awk '{print $8}')
+            cpu_usage=$(echo "$line" | awk '{print $5}')
+            memory_usage=$(echo "$line" | awk '{print $6}')
             # mem_temp=$(echo "$line" | awk '{print $4}')
-            # check_bytes $(echo "$line" | awk '{print $4}')
-            memory_bytes=$((check_bytes $(echo "$line" | awk '{print $4}')))
-            virt_memory_bytes=$(echo "$line" | awk '{print $3}')
+            # check_bytes $(echo "$line" | awk '{print $3}')
+            memory_bytes=$(check_bytes $(echo "$line" | awk '{print $4}'))
+            virt_memory_bytes=$(echo "$line" | awk '{print $2}')
             echo "$pid, $runtime, $cpu_usage, $memory_usage, $memory_bytes, $virt_memory_bytes" >> $csv_file ;
         done <<< "$ps_output"
-
+        totaltime=$((($(date +%s%N) /1000000) - $timestamp))
+        echo "process took $totaltime milliseconds"
         # Sleep for 0.1 seconds
         sleep 0.1
     done
-    totaltime=$((($(date +%s%N | cut -b1-13) - $start_time)))
+    totaltime=$((($(date +%s%N) /1000000)  - $start_time))
     echo "Total runtime of $isql_command is $totaltime milliseconds"
 }
 
-monitor_virtuoso $isql_clear_bulk $csv_iqsl_clear 9529
+# monitor_virtuoso $isql_clear_bulk $csv_iqsl_clear 9529
+# monitor_virtuoso $isql_delete_nmbs $csv_isql_del_nmbs 9529
+monitor_virtuoso $isql_load_nmbs $csv_isql_load_nmbs 9529
 # monitor_virtuoso $script_nmbs $csv_nmbs
 # monitor_virtuoso $script_dl1 $csv_dl1
 # monitor_virtuoso $script_dl2 $csv_dl2
